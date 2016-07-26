abfc904c50dde689e2cbb995a8af36b45d784e29
andywang646691 <64293427@qq.com>
2016-07-26 23:04:12 +0800

    task22-tabSwich

--- members/郑哲明/demo.js
+++ members/郑哲明/demo.js
@@ -1,4 +1,24 @@
-str = '.. <!-- My -- comment \n test --> ..  <!----> .. '
-re = /<!--[\s\S]*?-->/g
+arr = ["a", "b", 1, 3, 5, "b", 2];
+newarr = filterNumeric(arr);  //   [1,3,5,2]
 
-str.match(re) // '<!-- My -- comment \n test -->', '<!---->'
+function filterNumeric(arr) {
+	return arr.filter(function (e){
+		return typeof e === 'number'; 
+	})
+}
+
+
+=============非ES5实现==================
+    
+arr = ["a", "b", 1, 3, 5, "b", 2];
+newarr = filterNumeric(arr);  //   [1,3,5,2]
+    
+function filterNumeric(arr) {
+    var newArr = []
+    for (var i = 0 ; i < arr.length ; i ++) {
+        if (typeof arr[i] === 'number') {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
